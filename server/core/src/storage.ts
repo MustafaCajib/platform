da25fa244e8755bf08aa42d3c42656bcc9f7fdf5
Andrey Sobolev <haiodo@gmail.com>
2024-03-25 22:09:19 +0700

    UBERF-6161: Storage configuration
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- server/core/src/storage.ts
+++ server/core/src/storage.ts
@@ -1,50 +1,10 @@
-import { type WorkspaceId, toWorkspaceString } from '@hcengineering/core'
+import { type Blob, type MeasureContext, type WorkspaceId, toWorkspaceString } from '@hcengineering/core'
 import { type Readable } from 'stream'
-
-export interface MetadataItem {
-  Key: string
-  Value: string
-}
-export type BucketItem =
-  | {
-    name: string
-    size: number
-    etag: string
-    prefix?: never
-    lastModified: Date
-  }
-  | {
-    name?: never
-    etag?: never
-    lastModified?: never
-    prefix: string
-    size: 0
-  }
-
-export interface BucketItemStat {
-  size: number
-  etag: string
-  lastModified: Date
-  metaData: ItemBucketMetadata
-  versionId?: string | null
-}
 
 export interface UploadedObjectInfo {
   etag: string
   versionId: string | null
 }
-
-export interface ItemBucketMetadataList {
-  Items: MetadataItem[]
-}
-export type ItemBucketMetadata = Record<string, any>
-export type BucketItemWithMetadata = BucketItem & {
-  metadata?: ItemBucketMetadata | ItemBucketMetadataList
-}
-/**
- * @public
- */
-export type WorkspaceItem = Required<BucketItem> & { metaData: ItemBucketMetadata }
 
 /**
  * @public
@@ -54,21 +14,30 @@ export function getBucketId (workspaceId: WorkspaceId): string {
 }
 
 export interface StorageAdapter {
-  exists: (workspaceId: WorkspaceId) => Promise<boolean>
+  initialize: (ctx: MeasureContext, workspaceId: WorkspaceId) => Promise<void>
 
-  make: (workspaceId: WorkspaceId) => Promise<void>
-  remove: (workspaceId: WorkspaceId, objectNames: string[]) => Promise<void>
-  delete: (workspaceId: WorkspaceId) => Promise<void>
-  list: (workspaceId: WorkspaceId, prefix?: string) => Promise<WorkspaceItem[]>
-  stat: (workspaceId: WorkspaceId, objectName: string) => Promise<BucketItemStat>
-  get: (workspaceId: WorkspaceId, objectName: string) => Promise<Readable>
+  exists: (ctx: MeasureContext, workspaceId: WorkspaceId) => Promise<boolean>
+  make: (ctx: MeasureContext, workspaceId: WorkspaceId) => Promise<void>
+  delete: (ctx: MeasureContext, workspaceId: WorkspaceId) => Promise<void>
+
+  remove: (ctx: MeasureContext, workspaceId: WorkspaceId, objectNames: string[]) => Promise<void>
+  list: (ctx: MeasureContext, workspaceId: WorkspaceId, prefix?: string) => Promise<Blob[]>
+  stat: (ctx: MeasureContext, workspaceId: WorkspaceId, objectName: string) => Promise<Blob | undefined>
+  get: (ctx: MeasureContext, workspaceId: WorkspaceId, objectName: string) => Promise<Readable>
   put: (
+    ctx: MeasureContext,
     workspaceId: WorkspaceId,
     objectName: string,
     stream: Readable | Buffer | string,
-    size?: number,
-    metaData?: ItemBucketMetadata
+    contentType: string,
+    size?: number
   ) => Promise<UploadedObjectInfo>
-  read: (workspaceId: WorkspaceId, name: string) => Promise<Buffer[]>
-  partial: (workspaceId: WorkspaceId, objectName: string, offset: number, length?: number) => Promise<Readable>
+  read: (ctx: MeasureContext, workspaceId: WorkspaceId, name: string) => Promise<Buffer[]>
+  partial: (
+    ctx: MeasureContext,
+    workspaceId: WorkspaceId,
+    objectName: string,
+    offset: number,
+    length?: number
+  ) => Promise<Readable>
 }
